e05a70fd54b9294639a5c0cb72ecca3fb7a4233d
Rohan-Lohiya <rohanhero182004@gmail.com>
2024-08-07 00:37:46 +0530

    database.js

--- Backend/dbconnection/database.js
+++ Backend/dbconnection/database.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose")
-const db = "mongodb://localhost:27017/Take";
+const db = "mongodb+srv://rohanhero182004:E5xracSBNmK9Gvl6@cluster0.2gmsgug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Take";
 console.log(db);
 mongoose.connect(db).then(console.log(`db connected`)).catch((err) => console.log("err", err));
