230fce0bf00892ad463a4bd567d747ee8c16fffe
Rohan-Lohiya <rohanhero182004@gmail.com>
2024-08-07 16:29:08 +0530

    dicelogic.js

--- Backend/controller/dicelogic.js
+++ Backend/controller/dicelogic.js
@@ -1,4 +1,3 @@
-const express = require("express");
 const mongoose = require("mongoose");
 
 const TakeGames = require("../model/takegame");
@@ -18,7 +17,7 @@ const dicelogic = async (googleId, betamt, rollover, reverse) => {
       throw new Error("Insufficient balance");
     }
     if (betamt < 0) {
-        throw new Error("Cannot Bet with Negative Number");
+      throw new Error("Cannot Bet with Negative Number");
     }
 
     // Decrement the bet amount from the user's balance
@@ -28,6 +27,18 @@ const dicelogic = async (googleId, betamt, rollover, reverse) => {
     // Generate a random dice value between 0 and 100, up to 2 decimal places
     const randdice = parseFloat((Math.random() * 100).toFixed(2));
 
+    // Determine the profit based on game logic
+    let totalProfit = 0;
+    if (!reverse && randdice >= rollover) {
+      totalProfit = parseFloat((betamt * (100 / (100 - rollover))).toFixed(2));
+    } else if (reverse && randdice <= rollover) {
+      totalProfit = parseFloat((betamt * (100 / rollover)).toFixed(2));
+    }
+
+    // Update the user's balance and game state
+    user.balance += totalProfit;
+    await user.save(); // Save the updated balance
+
     // Save the user's game state to the database or create a new entry if not existing
     const userGame = await TakeGames.findOneAndUpdate(
       { GoogleID: googleId },
@@ -36,35 +47,18 @@ const dicelogic = async (googleId, betamt, rollover, reverse) => {
           betamount: betamt,
           dicerollover: rollover,
           diceval: randdice,
-          dicetotalprofit: 0,
+          dicetotalprofit: totalProfit,
         },
       },
       { upsert: true, new: true }
     );
 
-    // Calculate profit and update the game state
-    if (!reverse && randdice >= rollover) {
-      const totalProfit = parseFloat((betamt * (100 / (100 - rollover))).toFixed(2));
-      userGame.dicetotalprofit = totalProfit;
-      user.balance += totalProfit;
-      await user.save();
-    }
-    else if(reverse && randdice <= rollover){
-      const totalProfit = parseFloat((betamt * (100 / rollover)).toFixed(2));
-      userGame.dicetotalprofit = totalProfit;
-      user.balance += totalProfit;
-      await user.save();
-    } 
-    else {
-      userGame.dicetotalprofit = 0;
-    }
-
     await userGame.save(); // Save the updated game state
 
     return {
-        randdice,
-        balance: user.balance,
-      };
+      randdice,
+      balance: user.balance,
+    };
   } catch (error) {
     console.error(error.message);
     return { error: error.message };
