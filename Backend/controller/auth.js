c093be20703e7b292ad9084b304124d4773739f6
Rohan-Lohiya <rohanhero182004@gmail.com>
2024-08-18 21:13:05 +0530

    auth.js

--- Backend/controller/auth.js
+++ Backend/controller/auth.js
@@ -16,8 +16,8 @@ const secretKey = process.env.JWT_SECRET || "your-secret-key"; // Ensure this is
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_SECRET,
-      clientSecret: process.env.GOOGLE_CLIENT_ID,
+      clientID: process.env.,GOOGLE_CLIENT_ID
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
       scope: ["profile", "email"],
     },
