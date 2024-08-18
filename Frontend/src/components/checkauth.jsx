b11335acc7aee9632cc2daa280c4016205dc00c1
Rohan-Lohiya <rohanhero182004@gmail.com>
2024-08-18 16:26:55 +0530

    Add files via upload

---
+++ Frontend/src/components/checkauth.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const useCheckAuth = () => {
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const url = import.meta.env.VITE_API_URL;
+  //dotenv.config();
+
+  useEffect(() => {
+    const getuser = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/auth/login/success`, { withCredentials: true });
+        console.log("response", response);
+      } catch (error) {
+        navigate("/login");
+        console.log(url);
+      } finally {
+        setLoading(false);
+        
+      }
+    };
+
+    getuser();
+  }, [navigate]);
+
+  return loading;
+};
+
+export default useCheckAuth;
