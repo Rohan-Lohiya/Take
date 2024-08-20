a7ab027eeecdb930e6f3ec4de6120cf0a935f7c2
Rohan-Lohiya <rohanhero182004@gmail.com>
2024-08-20 23:16:49 +0530

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 3000;
 
 // CORS configuration
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://take-money-app.vercel.app",
   methods: "GET, POST, PUT, DELETE",
   credentials: true
 };
